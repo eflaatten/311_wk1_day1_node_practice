27d392d9364982b522d6f50952fe5c546d2970a7
eflaatten <elliotflaatten@live.com>
2024-05-28 22:43:52 -0500

    tests passing

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,15 +1,24 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  return true ? num % 2 === 0 : false
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  return arr.reduce((acc, curr) => acc + curr)
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+  for(let i=0; i<arr.length; i++){
+    for(let j=0; j<arr.length; j++){
+      if(arr[i] + arr[j] === sum){
+        return true
+      }
+    }
+  }
+  
+  return false
 
 }
 
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
-  // write code for strings.pairs
+  // write code for strings.pairs 
+  let result = []
+  for(let i=0; i<str.length; i+=2){
+    result.push(str.slice(i, i+2))
+  }
+  return result
+
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split('').reverse().join('')
 }
 
 module.exports = {
